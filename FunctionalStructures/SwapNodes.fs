b0ae095a940f0e476058dcc781d16fd5b3d2ebc0
Piotr Tecza <piotr.tecza@gmail.com>
2017-01-22 17:42:20 +0000

    building tree non functional way

--- FunctionalStructures/SwapNodes.fs
+++ FunctionalStructures/SwapNodes.fs
@@ -1,4 +1,5 @@
 module SwapNodes
+open System.Collections.Generic
 
 [<AllowNullLiteral>]
 type Node(x: int) = 
@@ -8,9 +9,9 @@ type Node(x: int) =
 
     member this.Left with get()= left
                       and set newValue = left <- newValue
-
     member this.Right with get()= right
                       and set newValue = right <- newValue
+    member this.Value = value
 
     member this.Swap() =
         let temp = left
@@ -19,15 +20,24 @@ type Node(x: int) =
 
 let BuildTree inputList= 
     let root = new Node(1)
-    let mutable nodeList = [root]
+    let nodeList = new List<Node>()
+    nodeList.Add(root);
+    let mutable index = 0;
 
-    match inputList with
-    | (l,r) -> 
-        if l <> -1 then 
-            let left = new Node(l)
-            nodeList.Head.Left <- left
-            nodeList <- nodeList.Tail @ [left]
-        if r <> -1 then 
-            let right = new Node(r)
-            nodeList.Head.Right <- right
-            nodeList <- nodeList.Tail @ [right]
+    for i in inputList do
+        let current = nodeList.[0]     
+        
+        if index % 2 = 0 then
+            if i > 0 then
+                current.Left <- new Node(i)
+                nodeList.Add(current.Left)
+            index <- index + 1                
+        else
+            if i > 0 then
+                current.Right <- new Node(i)
+                nodeList.Add(current.Right)
+            index <- index + 1
+            nodeList.RemoveAt(0)
+    root 
+
+
