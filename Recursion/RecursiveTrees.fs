26f6eedbb26e66ef518b6b10cb9ed27c45e26c09
Piotr Tecza <piotr.tecza@gmail.com>
2017-02-11 22:03:42 +0000

    working functional solution

--- Recursion/RecursiveTrees.fs
+++ Recursion/RecursiveTrees.fs
@@ -1,20 +1,21 @@
 module RecursiveTrees
 open System
 
-let arr = Array2D.init 20 20 (fun i j -> '_') 
 let printArray (arr:char[,]) = 
     for r in arr.GetLength(0)-1..-1..0 do
         for c in 0..arr.GetLength(1)-1 do
             printf "%c" arr.[r,c]
         printf "%s" Environment.NewLine
 
-printArray arr
-let createTree startColumn startRow length (arr:char[,]) =
-    for i in 0..length-1 do
-        arr.[startRow + i, startColumn] <- '1'
-    for i in 0..length-1 do
-        arr.[startRow + length + i, startColumn - i] <- '1'
-        arr.[startRow + length + i, startColumn + i] <- '1'
+let rec createTree startColumn startRow length level (arr:char[,]) :  char[,]=
+    if level > 0 then   
+        for i in 0..length-1 do
+            arr.[startRow + i, startColumn] <- '1'
+        for i in 0..length-1 do
+            arr.[startRow + length + i, startColumn - i - 1] <- '1'
+            arr.[startRow + length + i, startColumn + i + 1] <- '1'
+        createTree (startColumn-length) (startRow+length*2) (length/2) (level-1) arr |> ignore
+        createTree (startColumn+length) (startRow+length*2) (length/2) (level-1) arr |> ignore
     arr
 
-arr |> createTree 10 0 8 |> printArray
+Array2D.init 63 100 (fun i j -> '_') |> createTree 49 0 16 (Console.ReadLine()|>int) |> printArray
