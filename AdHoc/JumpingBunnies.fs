d73b07ad410891fdcbdf01fc47de0f7dcd7e8508
Piotr Tecza <piotr.tecza@gmail.com>
2017-03-25 15:49:22 +0000

    first working solution

--- AdHoc/JumpingBunnies.fs
+++ AdHoc/JumpingBunnies.fs
@@ -1,7 +1,22 @@
 module JumpingBunnies
+open System
+open System.Numerics
 
 let rec gcd (a:bigint) (b:bigint) = 
     if b = 0I then a
     else gcd b (a%b)
 
 let lcm (a:bigint) (b:bigint) = a * b / gcd a b
+
+let Run() = 
+    let bunniesCount = Console.ReadLine() |> int
+    let bunnies = 
+        Console.ReadLine().Split([|" "|], StringSplitOptions.RemoveEmptyEntries) |>
+        Array.map(fun x -> BigInteger.Parse(x))
+    let mutable result = 1I
+    for bunny in bunnies do
+        result <- lcm result bunny
+    printfn "%A" result
+
+Run()
+
