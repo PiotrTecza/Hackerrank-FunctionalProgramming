5de6aa65344dd97788d33b8d784b9400880946db
Piotr Tecza <piotr.tecza@gmail.com>
2017-03-26 22:19:02 +0100

    functional version

--- AdHoc/JumpingBunnies.fs
+++ AdHoc/JumpingBunnies.fs
@@ -2,21 +2,19 @@ module JumpingBunnies
 open System
 open System.Numerics
 
-let rec gcd (a:bigint) (b:bigint) = 
+let rec gcd a b = 
     if b = 0I then a
     else gcd b (a%b)
 
-let lcm (a:bigint) (b:bigint) = a * b / gcd a b
+let lcm a b = a * b / gcd a b
 
 let Run() = 
     let bunniesCount = Console.ReadLine() |> int
-    let bunnies = 
+    let results = 
         Console.ReadLine().Split([|" "|], StringSplitOptions.RemoveEmptyEntries) |>
-        Array.map(fun x -> BigInteger.Parse(x))
-    let mutable result = 1I
-    for bunny in bunnies do
-        result <- lcm result bunny
-    printfn "%A" result
+        Array.map(fun x -> BigInteger.Parse(x)) |>
+        Array.fold(fun acc x -> lcm acc x) 1I
+        
+    printfn "%A" results
 
 Run()
-
